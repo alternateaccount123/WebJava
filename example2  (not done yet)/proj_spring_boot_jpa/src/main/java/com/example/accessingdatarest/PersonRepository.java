d5062c4891028516b94e42e752c8c32551c964bd
alternateaccount123 <brandon_i_a@hotmail.com>
2022-12-17 15:36:48 -0800

    changed name of files

---
+++ example2  (not done yet)/proj_spring_boot_jpa/src/main/java/com/example/accessingdatarest/PersonRepository.java
@@ -0,0 +1,18 @@
+package com.example.accessingdatarest;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;  //added dependency "starter-data-rest"
+
+
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long>, CrudRepository<Person,Long> {
+
+  List<Person> findByLastName(@Param("name") String name);
+
+}
